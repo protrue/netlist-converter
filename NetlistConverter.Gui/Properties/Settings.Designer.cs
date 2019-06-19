75661cbb369b935c51cf1dd9105246bbfdcfd770
protrue <polecater@mail.ru>
2019-06-20 01:01:10 +0500

    Rename almost all

--- NetlistConverter.Gui/Properties/Settings.Designer.cs
+++ NetlistConverter.Gui/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Gui.Properties {
+namespace NetlistConverter.Gui.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
