2256d9f653261aef3a4b357c287e6860c85b1451
protrue <polecater@mail.ru>
2019-06-25 01:52:31 +0500

    Add class diagrams
    Change UI a little bit

--- NetlistConverter.Gui/FormMain.Designer.cs
+++ NetlistConverter.Gui/FormMain.Designer.cs
@@ -35,7 +35,6 @@
             this.textBoxOutputFilePath = new System.Windows.Forms.TextBox();
             this.buttonSetInputFile = new System.Windows.Forms.Button();
             this.buttonSetOutputFile = new System.Windows.Forms.Button();
-            this.buttonTranslate = new System.Windows.Forms.Button();
             this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
             this.tableLayoutPanelMain = new System.Windows.Forms.TableLayoutPanel();
@@ -52,13 +51,15 @@
             this.tabPageLog = new System.Windows.Forms.TabPage();
             this.richTextBoxLog = new System.Windows.Forms.RichTextBox();
             this.tabPageQuartusInstances = new System.Windows.Forms.TabPage();
-            this.tabPageQuartusNets = new System.Windows.Forms.TabPage();
             this.listBoxQuartusInstances = new System.Windows.Forms.ListBox();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPageQuartusNets = new System.Windows.Forms.TabPage();
             this.listBoxQuartusNets = new System.Windows.Forms.ListBox();
+            this.tabPageKochegInstances = new System.Windows.Forms.TabPage();
             this.listBoxKovchegInstances = new System.Windows.Forms.ListBox();
             this.tabPageKovchegNets = new System.Windows.Forms.TabPage();
             this.listBoxKovchegNets = new System.Windows.Forms.ListBox();
+            this.tableLayoutPanelConvert = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonTranslate = new System.Windows.Forms.Button();
             this.tableLayoutPanelMain.SuspendLayout();
             this.groupBoxQuartusFile.SuspendLayout();
             this.tableLayoutPanelQuartusFile.SuspendLayout();
@@ -71,8 +72,9 @@
             this.tabPageLog.SuspendLayout();
             this.tabPageQuartusInstances.SuspendLayout();
             this.tabPageQuartusNets.SuspendLayout();
-            this.tabPage1.SuspendLayout();
+            this.tabPageKochegInstances.SuspendLayout();
             this.tabPageKovchegNets.SuspendLayout();
+            this.tableLayoutPanelConvert.SuspendLayout();
             this.SuspendLayout();
             // 
             // menuStrip
@@ -109,9 +111,9 @@
             // 
             // buttonSetInputFile
             // 
-            this.buttonSetInputFile.Location = new System.Drawing.Point(3, 40);
+            this.buttonSetInputFile.Location = new System.Drawing.Point(3, 30);
             this.buttonSetInputFile.Name = "buttonSetInputFile";
-            this.buttonSetInputFile.Size = new System.Drawing.Size(100, 25);
+            this.buttonSetInputFile.Size = new System.Drawing.Size(100, 22);
             this.buttonSetInputFile.TabIndex = 7;
             this.buttonSetInputFile.Text = "Выбрать";
             this.buttonSetInputFile.UseVisualStyleBackColor = true;
@@ -119,23 +121,13 @@
             // 
             // buttonSetOutputFile
             // 
-            this.buttonSetOutputFile.Location = new System.Drawing.Point(3, 40);
+            this.buttonSetOutputFile.Location = new System.Drawing.Point(3, 30);
             this.buttonSetOutputFile.Name = "buttonSetOutputFile";
-            this.buttonSetOutputFile.Size = new System.Drawing.Size(100, 25);
+            this.buttonSetOutputFile.Size = new System.Drawing.Size(100, 22);
             this.buttonSetOutputFile.TabIndex = 8;
             this.buttonSetOutputFile.Text = "Выбрать";
             this.buttonSetOutputFile.UseVisualStyleBackColor = true;
             this.buttonSetOutputFile.Click += new System.EventHandler(this.ButtonSetOutputFileClick);
-            // 
-            // buttonTranslate
-            // 
-            this.buttonTranslate.Location = new System.Drawing.Point(3, 203);
-            this.buttonTranslate.Name = "buttonTranslate";
-            this.buttonTranslate.Size = new System.Drawing.Size(150, 30);
-            this.buttonTranslate.TabIndex = 9;
-            this.buttonTranslate.Text = "Конвертировать";
-            this.buttonTranslate.UseVisualStyleBackColor = true;
-            this.buttonTranslate.Click += new System.EventHandler(this.ButtonTranslateClick);
             // 
             // openFileDialog
             // 
@@ -150,15 +142,15 @@
             this.tableLayoutPanelMain.ColumnCount = 1;
             this.tableLayoutPanelMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanelMain.Controls.Add(this.groupBoxQuartusFile, 0, 0);
-            this.tableLayoutPanelMain.Controls.Add(this.buttonTranslate, 0, 2);
             this.tableLayoutPanelMain.Controls.Add(this.groupBoxKovchegFile, 0, 1);
             this.tableLayoutPanelMain.Controls.Add(this.groupBoxView, 0, 3);
+            this.tableLayoutPanelMain.Controls.Add(this.tableLayoutPanelConvert, 0, 2);
             this.tableLayoutPanelMain.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanelMain.Location = new System.Drawing.Point(0, 24);
             this.tableLayoutPanelMain.Name = "tableLayoutPanelMain";
             this.tableLayoutPanelMain.RowCount = 4;
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
-            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
             this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
             this.tableLayoutPanelMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
             this.tableLayoutPanelMain.Size = new System.Drawing.Size(784, 515);
@@ -170,7 +162,7 @@
             this.groupBoxQuartusFile.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBoxQuartusFile.Location = new System.Drawing.Point(3, 3);
             this.groupBoxQuartusFile.Name = "groupBoxQuartusFile";
-            this.groupBoxQuartusFile.Size = new System.Drawing.Size(778, 94);
+            this.groupBoxQuartusFile.Size = new System.Drawing.Size(778, 74);
             this.groupBoxQuartusFile.TabIndex = 0;
             this.groupBoxQuartusFile.TabStop = false;
             this.groupBoxQuartusFile.Text = "Файл схемы из Quartus";
@@ -187,16 +179,16 @@
             this.tableLayoutPanelQuartusFile.RowCount = 2;
             this.tableLayoutPanelQuartusFile.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanelQuartusFile.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelQuartusFile.Size = new System.Drawing.Size(772, 75);
+            this.tableLayoutPanelQuartusFile.Size = new System.Drawing.Size(772, 55);
             this.tableLayoutPanelQuartusFile.TabIndex = 8;
             // 
             // groupBoxKovchegFile
             // 
             this.groupBoxKovchegFile.Controls.Add(this.tableLayoutPanelKovchegFile);
             this.groupBoxKovchegFile.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxKovchegFile.Location = new System.Drawing.Point(3, 103);
+            this.groupBoxKovchegFile.Location = new System.Drawing.Point(3, 83);
             this.groupBoxKovchegFile.Name = "groupBoxKovchegFile";
-            this.groupBoxKovchegFile.Size = new System.Drawing.Size(778, 94);
+            this.groupBoxKovchegFile.Size = new System.Drawing.Size(778, 74);
             this.groupBoxKovchegFile.TabIndex = 1;
             this.groupBoxKovchegFile.TabStop = false;
             this.groupBoxKovchegFile.Text = "Файл схемы для Ковчег";
@@ -213,16 +205,16 @@
             this.tableLayoutPanelKovchegFile.RowCount = 2;
             this.tableLayoutPanelKovchegFile.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanelKovchegFile.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanelKovchegFile.Size = new System.Drawing.Size(772, 75);
+            this.tableLayoutPanelKovchegFile.Size = new System.Drawing.Size(772, 55);
             this.tableLayoutPanelKovchegFile.TabIndex = 9;
             // 
             // groupBoxView
             // 
             this.groupBoxView.Controls.Add(this.tabControlView);
             this.groupBoxView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.groupBoxView.Location = new System.Drawing.Point(3, 243);
+            this.groupBoxView.Location = new System.Drawing.Point(3, 203);
             this.groupBoxView.Name = "groupBoxView";
-            this.groupBoxView.Size = new System.Drawing.Size(778, 269);
+            this.groupBoxView.Size = new System.Drawing.Size(778, 309);
             this.groupBoxView.TabIndex = 2;
             this.groupBoxView.TabStop = false;
             this.groupBoxView.Text = "Просмотр";
@@ -234,13 +226,13 @@
             this.tabControlView.Controls.Add(this.tabPageLog);
             this.tabControlView.Controls.Add(this.tabPageQuartusInstances);
             this.tabControlView.Controls.Add(this.tabPageQuartusNets);
-            this.tabControlView.Controls.Add(this.tabPage1);
+            this.tabControlView.Controls.Add(this.tabPageKochegInstances);
             this.tabControlView.Controls.Add(this.tabPageKovchegNets);
             this.tabControlView.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControlView.Location = new System.Drawing.Point(3, 16);
             this.tabControlView.Name = "tabControlView";
             this.tabControlView.SelectedIndex = 0;
-            this.tabControlView.Size = new System.Drawing.Size(772, 250);
+            this.tabControlView.Size = new System.Drawing.Size(772, 290);
             this.tabControlView.TabIndex = 0;
             // 
             // tabPageQuartus
@@ -249,7 +241,7 @@
             this.tabPageQuartus.Location = new System.Drawing.Point(4, 22);
             this.tabPageQuartus.Name = "tabPageQuartus";
             this.tabPageQuartus.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPageQuartus.Size = new System.Drawing.Size(764, 224);
+            this.tabPageQuartus.Size = new System.Drawing.Size(764, 274);
             this.tabPageQuartus.TabIndex = 0;
             this.tabPageQuartus.Text = "Схема из Quartus";
             this.tabPageQuartus.UseVisualStyleBackColor = true;
@@ -260,7 +252,7 @@
             this.richTextBoxQuartusScheme.Location = new System.Drawing.Point(3, 3);
             this.richTextBoxQuartusScheme.Name = "richTextBoxQuartusScheme";
             this.richTextBoxQuartusScheme.ReadOnly = true;
-            this.richTextBoxQuartusScheme.Size = new System.Drawing.Size(758, 218);
+            this.richTextBoxQuartusScheme.Size = new System.Drawing.Size(758, 268);
             this.richTextBoxQuartusScheme.TabIndex = 0;
             this.richTextBoxQuartusScheme.Text = "";
             // 
@@ -316,6 +308,15 @@
             this.tabPageQuartusInstances.Text = "Модули Quartus";
             this.tabPageQuartusInstances.UseVisualStyleBackColor = true;
             // 
+            // listBoxQuartusInstances
+            // 
+            this.listBoxQuartusInstances.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBoxQuartusInstances.FormattingEnabled = true;
+            this.listBoxQuartusInstances.Location = new System.Drawing.Point(3, 3);
+            this.listBoxQuartusInstances.Name = "listBoxQuartusInstances";
+            this.listBoxQuartusInstances.Size = new System.Drawing.Size(758, 218);
+            this.listBoxQuartusInstances.TabIndex = 0;
+            // 
             // tabPageQuartusNets
             // 
             this.tabPageQuartusNets.Controls.Add(this.listBoxQuartusNets);
@@ -327,26 +328,6 @@
             this.tabPageQuartusNets.Text = "Соединения Quartus";
             this.tabPageQuartusNets.UseVisualStyleBackColor = true;
             // 
-            // listBoxQuartusInstances
-            // 
-            this.listBoxQuartusInstances.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBoxQuartusInstances.FormattingEnabled = true;
-            this.listBoxQuartusInstances.Location = new System.Drawing.Point(3, 3);
-            this.listBoxQuartusInstances.Name = "listBoxQuartusInstances";
-            this.listBoxQuartusInstances.Size = new System.Drawing.Size(758, 218);
-            this.listBoxQuartusInstances.TabIndex = 0;
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this.listBoxKovchegInstances);
-            this.tabPage1.Location = new System.Drawing.Point(4, 22);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(764, 224);
-            this.tabPage1.TabIndex = 5;
-            this.tabPage1.Text = "Модули Ковчег";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
             // listBoxQuartusNets
             // 
             this.listBoxQuartusNets.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -356,13 +337,24 @@
             this.listBoxQuartusNets.Size = new System.Drawing.Size(758, 218);
             this.listBoxQuartusNets.TabIndex = 0;
             // 
+            // tabPageKochegInstances
+            // 
+            this.tabPageKochegInstances.Controls.Add(this.listBoxKovchegInstances);
+            this.tabPageKochegInstances.Location = new System.Drawing.Point(4, 22);
+            this.tabPageKochegInstances.Name = "tabPageKochegInstances";
+            this.tabPageKochegInstances.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageKochegInstances.Size = new System.Drawing.Size(764, 264);
+            this.tabPageKochegInstances.TabIndex = 5;
+            this.tabPageKochegInstances.Text = "Модули Ковчег";
+            this.tabPageKochegInstances.UseVisualStyleBackColor = true;
+            // 
             // listBoxKovchegInstances
             // 
             this.listBoxKovchegInstances.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBoxKovchegInstances.FormattingEnabled = true;
             this.listBoxKovchegInstances.Location = new System.Drawing.Point(3, 3);
             this.listBoxKovchegInstances.Name = "listBoxKovchegInstances";
-            this.listBoxKovchegInstances.Size = new System.Drawing.Size(758, 218);
+            this.listBoxKovchegInstances.Size = new System.Drawing.Size(758, 258);
             this.listBoxKovchegInstances.TabIndex = 0;
             // 
             // tabPageKovchegNets
@@ -384,6 +376,31 @@
             this.listBoxKovchegNets.Name = "listBoxKovchegNets";
             this.listBoxKovchegNets.Size = new System.Drawing.Size(758, 218);
             this.listBoxKovchegNets.TabIndex = 0;
+            // 
+            // tableLayoutPanelConvert
+            // 
+            this.tableLayoutPanelConvert.ColumnCount = 3;
+            this.tableLayoutPanelConvert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelConvert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelConvert.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanelConvert.Controls.Add(this.buttonTranslate, 1, 0);
+            this.tableLayoutPanelConvert.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelConvert.Location = new System.Drawing.Point(3, 163);
+            this.tableLayoutPanelConvert.Name = "tableLayoutPanelConvert";
+            this.tableLayoutPanelConvert.RowCount = 1;
+            this.tableLayoutPanelConvert.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelConvert.Size = new System.Drawing.Size(778, 34);
+            this.tableLayoutPanelConvert.TabIndex = 3;
+            // 
+            // buttonTranslate
+            // 
+            this.buttonTranslate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.buttonTranslate.Location = new System.Drawing.Point(262, 3);
+            this.buttonTranslate.Name = "buttonTranslate";
+            this.buttonTranslate.Size = new System.Drawing.Size(253, 28);
+            this.buttonTranslate.TabIndex = 10;
+            this.buttonTranslate.Text = "Конвертировать";
+            this.buttonTranslate.UseVisualStyleBackColor = true;
             // 
             // FormMain
             // 
@@ -412,8 +429,9 @@
             this.tabPageLog.ResumeLayout(false);
             this.tabPageQuartusInstances.ResumeLayout(false);
             this.tabPageQuartusNets.ResumeLayout(false);
-            this.tabPage1.ResumeLayout(false);
+            this.tabPageKochegInstances.ResumeLayout(false);
             this.tabPageKovchegNets.ResumeLayout(false);
+            this.tableLayoutPanelConvert.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -427,7 +445,6 @@
         private System.Windows.Forms.TextBox textBoxOutputFilePath;
         private System.Windows.Forms.Button buttonSetInputFile;
         private System.Windows.Forms.Button buttonSetOutputFile;
-        private System.Windows.Forms.Button buttonTranslate;
         private System.Windows.Forms.OpenFileDialog openFileDialog;
         private System.Windows.Forms.SaveFileDialog saveFileDialog;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanelMain;
@@ -446,11 +463,13 @@
         private System.Windows.Forms.TabPage tabPageQuartusInstances;
         private System.Windows.Forms.TabPage tabPageQuartusNets;
         private System.Windows.Forms.ListBox listBoxQuartusInstances;
-        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPageKochegInstances;
         private System.Windows.Forms.ListBox listBoxQuartusNets;
         private System.Windows.Forms.ListBox listBoxKovchegInstances;
         private System.Windows.Forms.TabPage tabPageKovchegNets;
         private System.Windows.Forms.ListBox listBoxKovchegNets;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelConvert;
+        private System.Windows.Forms.Button buttonTranslate;
     }
 }
 
